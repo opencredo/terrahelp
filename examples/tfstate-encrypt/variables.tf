8aca0b1e3731154baf150d864c5391e9fa702ee0
Hieu Doan <hiieu@users.noreply.github.com>
2021-07-28 10:38:32 +0100

    Remove quoted types in variables for tf > 0.11 support

--- examples/tfstate-encrypt/variables.tf
+++ examples/tfstate-encrypt/variables.tf
@@ -9,11 +9,12 @@ variable "tf_sensitive_key_3"     {}
 variable "tf_sensitive_key_4"     {}
 variable "tf_sensitive_key_6"     {}
 
-variable "tf_sensitive_list_vals"       { type = "list" }
-variable "tf_sensitive_flatmap_vals"    { type = "map"  }
+variable "tf_sensitive_list_vals"       { type = list(string) }
+variable "tf_sensitive_flatmap_vals"    { type = map(string)  }
 
 # ----------------------------------------------------------------
 # Non sensitive variable defaults
 # ----------------------------------------------------------------
 variable "tf_normal_key_1"     { default = "normal value 1" }
 variable "tf_normal_key_2"     { default = "normal value 2" }
+
